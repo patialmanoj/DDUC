13c0f593cf447993ce3ed8bcc347dd841523d298
Patial_Digger <patial.du.cs@gmail.com>
2020-01-25 14:12:21 +0530

    With adding raw  slider

--- js/news.js
+++ js/news.js
@@ -1,12 +1,44 @@
 
 var  newsPlugin = (function(){
-    var data = undefined;
+    var data = undefined,
+    graphData = undefined;
     function init(){
         console.log("plugin module is init ");
+        $('.slider ul').data("marginleft",0);
+        $('.slider ul').data("marginright",900);
+        
         data = DataManager.sportsData;
-        console.log(DataManager.sportsData);
+        graphData = DataManager.graphData;
+        console.log(graphData);
         appendBigNews();
+        createNewsGraph();
+        bindClicks();
+        
     }
+    function bindClicks(){
+      $('.slider_button').on("click",function(event){
+        var  $el= $(event.target);
+        var $slider= $('.slider ul');
+        var $elBttn = $el.data("btn");
+        var margin =  $slider.data("marginleft") ;
+          
+        if($elBttn == 'left'){
+          margin = margin- 200;
+          $slider.css("margin-left", margin);
+          $slider.data("marginleft",margin);
+        }
+        else{
+          if(margin != 0){
+            margin = margin + 200;
+            $slider.css("margin-left", margin);
+            $slider.data("marginleft",margin); 
+          }
+        }
+      });
+
+    }
+
+
     function appendBigNews(){
         var html = createTheBigNews();
         $('.bigNews ul').append(html);
@@ -32,6 +64,59 @@ var  newsPlugin = (function(){
                     '</li>'
         }
         return html;
+    }
+    function createNewsGraph(){
+        Highcharts.chart('graph-listing', {
+            chart: {
+              type: 'column'
+            },
+            title: {
+              text: 'News Portal Statistics. January, 2020'
+            },
+            subtitle: {
+              text: 'Click the columns to view versions. Source: <a href="https://timesofindia.indiatimes.com" target="_blank">Anonymous Data just for training </a>'
+            },
+            accessibility: {
+              announceNewData: {
+                enabled: false
+              }
+            },
+            xAxis: {
+              type: 'News Portal'
+            },
+            yAxis: {
+              title: {
+                text: 'Total news distribution share'
+              }
+            },
+            legend: {
+              enabled: false
+            },
+            plotOptions: {
+              series: {
+                borderWidth: 0,
+                dataLabels: {
+                  enabled: true,
+                  format: '{point.y:.1f}%'
+                }
+              }
+            },
+          
+            tooltip: {
+              headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+              pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+            },
+          
+            series: [{
+              name: "NEWS DATA",
+              colorByPoint: true,
+              data: graphData.mainGraphData,
+            }],
+            drilldown: {
+              series: graphData.drillDownData
+            }
+          });
+          
     }
 
     return {
